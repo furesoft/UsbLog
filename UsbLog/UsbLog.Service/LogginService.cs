a131fb0950286ba77e766a285dbdb2e21147a9a9
Furesoft <filmee24@gmail.com>
2020-02-14 15:41:23 +0100

    initial project commit

---
+++ UsbLog/UsbLog.Service/LogginService.cs
@@ -0,0 +1,17 @@
+﻿using Topshelf;
+
+namespace UsbLog.Service
+{
+    public class LogginService : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ UsbLog/UsbLog.Service/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Topshelf;
+
+namespace UsbLog.Service
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<LogginService>();
+                x.EnableServiceRecovery(r => r.RestartService(TimeSpan.FromSeconds(10)));
+                x.SetServiceName("UsbLog");
+                x.SetDescription("UsbLog  Service");
+                x.StartAutomatically();
+                x.UseNLog();
+            });
+        }
+    }
+}
