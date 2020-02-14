2106acd1b8d8685c8273655d79cd61d4f07ab879
Furesoft <filmee24@gmail.com>
2020-02-14 15:52:43 +0100

    added helper class for usb events

---
+++ UsbLog/UsbLog.Core/UsbEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Management;
+
+namespace UsbLog.Core
+{
+    internal class UsbEvent
+    {
+        public static void OnAttach(Action<string> callback)
+        {
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler((_, e) => callback(e.NewEvent.Properties["DriveName"].ToString()));
+            insertWatcher.Start();
+        }
+
+        public static void OnDettach(Action<string> callback)
+        {
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler((_, e) => callback(e.NewEvent.Properties["DriveName"].ToString()));
+            removeWatcher.Start();
+        }
+    }
+}
