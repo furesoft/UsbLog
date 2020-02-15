d8bd0737ec138f7a54ec3f3438c4f82d17181023
Furesoft <filmee24@gmail.com>
2020-02-15 17:12:01 +0100

    cleanup

--- UsbLog/UsbLog.Service/Middleware/InitMiddleware.cs
+++ UsbLog/UsbLog.Service/Middleware/InitMiddleware.cs
@@ -20,8 +20,6 @@ namespace UsbLog.Service.Middleware
             var buffer = DISK.ReadBytes(0, 2048, strm.Strm);
 
             strm.Hive = new RegistryHive(strm.Strm.STR);
-            var hello = strm.Hive.Root.OpenSubKey("hello world");
-            var value = hello.GetValue("data");
 
             next(strm);
         }
