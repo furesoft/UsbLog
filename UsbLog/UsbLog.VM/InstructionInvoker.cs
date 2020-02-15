b9c73eb251af40ee829eba2c1bf3bb8b66a18b69
Furesoft <filmee24@gmail.com>
2020-02-15 10:04:20 +0100

    added instructioninvoker

---
+++ UsbLog/UsbLog.VM/InstructionInvoker.cs
@@ -0,0 +1,28 @@
+﻿using DiscUtils.Registry;
+using System.Collections.Generic;
+
+namespace UsbLog.VM
+{
+    public class InstructionInvoker
+    {
+        public static void AddTask(IVmTask task)
+        {
+            _tasks.Add(task.ID, task);
+        }
+
+        public static void Invoke(byte[] bytecode, RegistryKey root)
+        {
+            byte opcode = 0;
+            int iP = -1;
+
+            while (opcode != 0x1)
+            {
+                opcode = bytecode[iP++];
+
+                _tasks[opcode].Invoke(root);
+            }
+        }
+
+        private static Dictionary<int, IVmTask> _tasks = new Dictionary<int, IVmTask>();
+    }
+}
