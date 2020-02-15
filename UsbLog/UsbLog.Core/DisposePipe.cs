53c42576d35e5fa3af1fcb925ea80a6a658908e0
Furesoft <filmee24@gmail.com>
2020-02-15 09:45:37 +0100

    added disposepipe

---
+++ UsbLog/UsbLog.Core/DisposePipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsbLog.Core
+{
+    public static class DisposePipe
+    {
+        public static void Add(IDisposable disposable)
+        {
+            _disposables.Add(disposable);
+        }
+
+        public static void DisposeAll()
+        {
+            foreach (var d in _disposables)
+            {
+                d.Dispose();
+            }
+        }
+
+        private static List<IDisposable> _disposables = new List<IDisposable>();
+    }
+}
