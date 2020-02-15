09d3584a145dba5cc8a15f72b5ca6d6c6e16974c
Furesoft <filmee24@gmail.com>
2020-02-15 09:09:18 +0100

    added taskqueue

---
+++ UsbLog/UsbLog.Core/TaskQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsbLog.Core
+{
+    public static class TaskQueue
+    {
+        public static void Add(int id, Action action)
+        {
+            if (!_tasks.ContainsKey(id))
+            {
+                _tasks.Add(id, action);
+            }
+        }
+
+        public static void RunAllTasks()
+        {
+            foreach (var t in _tasks)
+            {
+                t.Value();
+            }
+        }
+
+        public static void RunTask(int id)
+        {
+            _tasks[id]();
+        }
+
+        private static Dictionary<int, Action> _tasks = new Dictionary<int, Action>();
+    }
+}
